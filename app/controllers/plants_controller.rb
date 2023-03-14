7ed30f0f4346e8441173fada9ea8478ae3d6a5f6
+ <+>
2023-03-14 20:48:40 +0300

    commit tree 8cd6a605410cfe93725cef01cf4e9103f1eb04dc

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,29 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+    def show
+        plant = Plant.find_by(id:params[:id])
+        if plant
+          render json: plant
+         else
+          render json: { error: "plants  not found" }, status: :not_found
+         end
+    end
+    def create
+        plant =  Plant.create(plant_params)
+        if plant.valid?
+        render json: plant, status: :created
+        else 
+            render json: {error:plant.errors.full_messages }, status: :unprocessable_entity
+        end
+
+    end
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
+
+
+
